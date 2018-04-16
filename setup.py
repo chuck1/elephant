9a0bcdba636430a59e1b71f80816c911b315cab3
Charles Rymal <charles.rymal@nortek.com>
2018-04-16 12:20:10 -0700

    add setup and some scripts

---
+++ setup.py
@@ -0,0 +1,16 @@
+import re
+from setuptools import setup
+
+setup(name='elephant',
+        version='0.1',
+        description='',
+        url='',
+        author='Charles Rymal',
+        author_email='charlesrymal@gmail.com',
+        license='MIT',
+        packages=[
+            'elephant',
+            ],
+        zip_safe=False,
+        )
+
