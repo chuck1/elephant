8d6c94164820b45bdaaeff84d3d84d39c0e531cf
Charles Rymal <charlesrymal@gmail.com>
2018-10-09 19:49:07 +0000

    minor

--- elephant/__init__.py
+++ elephant/__init__.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 class Engine:
 
     async def _factory(self, *args):
+
         d = args[0]
         try:
             o = self._doc_class(self, await self.h.decode(d), d, *args[1:])
