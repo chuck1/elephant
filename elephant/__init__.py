59c39fee02f67df6ac050d63e969bb7551d704f4
Charles Rymal <charlesrymal@gmail.com>
2019-01-09 22:10:25 +0000

    minor

--- elephant/__init__.py
+++ elephant/__init__.py
@@ -12,12 +12,15 @@ class Engine:
     async def _factory(self, *args):
 
         d = args[0]
+
         try:
             o = self._doc_class(self, await self.h.decode(d), d, *args[1:])
         except Exception as e:
             logger.error("failed to create {self._doc_class}")
             raise
+
         o.h = self.h
+
         return o
 
 
